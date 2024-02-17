eb1e5c8f7d872e5ae9cba71238161e40a6c1e186
sebash3151 <sebash31510@gmail.com>
2024-02-16 20:06:35 -0500

    hope

---
+++ CocinitaEnVR/Assets/Scritps/SpeedCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedCalculator : MonoBehaviour
+{
+    public float speed;
+    public float limitSpeedToExplode = 3;
+
+    void Update()
+    {
+        StartCoroutine(CalculateSpeed());
+    }
+    IEnumerator CalculateSpeed() 
+    {
+        Vector3 lastPosition = transform.position;
+        yield return new WaitForFixedUpdate();
+        speed = (lastPosition - transform.position).magnitude / Time.deltaTime;
+    }
+}
